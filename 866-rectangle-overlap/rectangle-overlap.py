4d8e32102e6a0245dbbf51cf4ff7c0f8660487e4
guduswain07-lang <swainrudra61@gmail.com>
2026-09-14 22:26:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.89%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isRectangleOverlap(self, rec1, rec2):
+        """
+        :type rec1: List[int]
+        :type rec2: List[int]
+        :rtype: bool
+        """
+        return (min(rec1[2], rec2[2]) > max(rec1[0], rec2[0]) and
+                min(rec1[3], rec2[3]) > max(rec1[1], rec2[1]))
