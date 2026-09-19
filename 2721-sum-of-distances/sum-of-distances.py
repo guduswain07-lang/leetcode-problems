3bdb85653743851015928247b05c6be81852473c
guduswain07-lang <swainrudra61@gmail.com>
2026-09-19 22:03:05 +0530

    Time: 246 ms (19.57%) | Memory: 36.3 MB (99.28%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.py
+++ 2721-sum-of-distances/sum-of-distances.py
@@ -1,38 +1,31 @@
-class Solution:
-    def distance(self, nums):
-        n = len(nums)
-        ans = [0] * n
+from collections import defaultdict
 
-        count = {}
-        total = {}
 
-        # Left side contribution
-        for i in range(n):
-            x = nums[i]
+class Solution(object):
 
-            if x not in count:
-                count[x] = 0
-                total[x] = 0
+  def distance(self, nums):
+    """:type nums: List[int]
 
-            ans[i] += count[x] * i - total[x]
+    :rtype: List[int]
+    """
+    n = len(nums)
+    ans = [0] * n
 
-            count[x] += 1
-            total[x] += i
+    # Left pass: Calculate distance contribution from elements to the left
+    count = defaultdict(int)
+    total = defaultdict(int)
+    for i, x in enumerate(nums):
+      ans[i] += count[x] * i - total[x]
+      count[x] += 1
+      total[x] += i
 
-        # Right side contribution
-        count = {}
-        total = {}
+    # Right pass: Calculate distance contribution from elements to the right
+    count.clear()
+    total.clear()
+    for i in range(n - 1, -1, -1):
+      x = nums[i]
+      ans[i] += total[x] - count[x] * i
+      count[x] += 1
+      total[x] += i
 
-        for i in range(n - 1, -1, -1):
-            x = nums[i]
-
-            if x not in count:
-                count[x] = 0
-                total[x] = 0
-
-            ans[i] += total[x] - count[x] * i
-
-            count[x] += 1
-            total[x] += i
-
-        return ans
+    return ans
